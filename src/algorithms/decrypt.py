afcdaedfebedd5d6b1fafd5422d71fcf0785a930
esquivelgor <esquivelguillerm@gmail.com>
2023-09-17 02:10:58 -0600

    test

--- src/algorithms/decrypt.py
+++ src/algorithms/decrypt.py
@@ -21,14 +21,13 @@ def decrypt_message(encrypted_message: str, key: str) -> str:
         operator = binary
         for char_key in key_ascii:
             confussion_decrypt_result = _confussion_decrypt(operator, char_key, len(key))
+            difussion_decrypt_result = _diffusion_decrypt(operator, char_key)
             
-            # TODO: Call Difussion Decrypt here with the result of the confussion decrypt
-            # Then assign the result of the difussion to the operator
-           
             operator = confussion_decrypt_result
-        
+
+
         decrypted_in_binary_ascii.append(operator)    
-    
+
     decrypted_plain_text = ""
     for binary_ascii in decrypted_in_binary_ascii:
         decrypted_plain_text += transform_ascii_to_char(binary_ascii)
@@ -43,8 +42,23 @@ def _confussion_decrypt(binary_char1: str, binary_char2: str, key_length: int) -
     
     return xor_result
 
-def _difussion_decrypt(encrypted_message: str, key: str) -> str:
-    """
-    TODO
-    """
-    pass
+def _diffusion_decrypt(encrypted_message: str, key: str):
+    # Number of left shifts
+    nrs = str(int(key, 2))
+    shifts = 0
+    for i in range(0, len(nrs)):
+        if (shifts <= 7):
+            shifts += int(nrs[i])
+
+    shifts = (shifts if shifts <= 8 else shifts - 8)
+
+    decimal_number_shifted = int(encrypted_message, 2)
+    
+    decimal_number = decimal_number_shifted << shifts
+
+    binary_msg = bin(decimal_number)
+    print(shifts)
+    print(encrypted_message)
+    print(binary_msg)
+    print("")
+    return encrypted_message
--- src/algorithms/encrypt.py
+++ src/algorithms/encrypt.py
@@ -23,11 +23,11 @@ def encrypt_message(plain_text: str, key: str) -> str:
         operator = char_message
         for char_key in key_ascii:
             confussion_result_encrypt = _confussion_encrypt(operator, char_key, len(key))
-            # TODO: Call Difussion decrypt here with the result of the confussion
-            # Then assign the result of the difussion to the operator
-            operator = confussion_result_encrypt
+            difussion_result_encrypt = _difussion_encrypt(confussion_result_encrypt, char_key)
             
-        binary_array_result.append(confussion_result_encrypt)
+            operator = difussion_result_encrypt
+
+        binary_array_result.append(operator)
         
     base64_result = base64.b64encode(str(binary_array_result).encode('utf-8'))
     
@@ -36,12 +36,29 @@ def encrypt_message(plain_text: str, key: str) -> str:
 def _confussion_encrypt(binary_char1: str, binary_char2: str, key_length: int):
     xor_result = xor_binary_values(binary_char1, binary_char2)
     multiplied_result = multiply_binary(xor_result, key_length)
-    
     return multiplied_result
         
 
-def _difussion_encrypt(plain_text: str, key: str) -> str:
-    """
-    TODO
-    """
-    pass
+def _difussion_encrypt(binary_msg: str, key_char: str):
+    # Number of right shifts 
+    nrs = str(int(key_char, 2))
+    shifts = 0
+    
+    for i in range(0, len(nrs)):
+        if (shifts <= 7):
+            shifts += int(nrs[i])
+    # (if n < 0 then shifting by 2 positions)
+    shifts = (shifts if shifts <= 8 else shifts - 8)
+
+    # Convert binary string to an integer
+    decimal_number = int(binary_msg, 2)
+    # Perform a bitwise shift right operation 
+    shifted_decimal = decimal_number >> shifts
+    # Convert the shifted decimal number back to a binary string
+    shifted_binary = bin(shifted_decimal)
+
+    print(shifts)
+    print(binary_msg)
+    print(shifted_binary)
+    print("")
+    return shifted_binary
