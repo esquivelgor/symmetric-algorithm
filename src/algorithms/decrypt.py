8d6a6ed23141c4032f08e78040bfbe91a7261e81
esquivelgor <esquivelguillerm@gmail.com>
2023-09-18 20:47:28 -0600

    2nd diffusion method done

--- src/algorithms/decrypt.py
+++ src/algorithms/decrypt.py
@@ -12,7 +12,7 @@ def decrypt_message(encrypted_message: str, key: str) -> str:
     binary_array = binary_array_string.replace("[", "").replace("]", "").replace("'", "").split(", ")
     
     key_ascii = []
-    decrypted_in_binary_ascii = []
+    binary_ascii = []
     
     for char in reversed_key:
         key_ascii.append(transform_char_to_ascii(char))
@@ -21,21 +21,18 @@ def decrypt_message(encrypted_message: str, key: str) -> str:
         operator = binary
         for char_key in key_ascii:
             confussion_decrypt_result = _confussion_decrypt(operator, char_key, len(key))
-            difussion_decrypt_result = _diffusion_decrypt_shift(confussion_decrypt_result, char_key)
-
-            #print(confussion_decrypt_result)
-            #print(difussion_decrypt_result)
+            
             operator = confussion_decrypt_result # debe de ir diffusion_decrypt_result
 
-        decrypted_in_binary_ascii.append(operator)   
-
-    #difussion_decrypt_result.append(_diffusion_decrypt_rotation(decrypted_in_binary_ascii, key))
+        binary_ascii.append(operator)   
+    result_decrypt = _diffusion_encrypt_transposition(binary_ascii, key)
+    
     decrypted_plain_text = ""
-    for binary_ascii in decrypted_in_binary_ascii:
+    for binary_ascii in result_decrypt:
         decrypted_plain_text += transform_ascii_to_char(binary_ascii)
     
     
-    return decrypted_in_binary_ascii, decrypted_plain_text
+    return result_decrypt, decrypted_plain_text
     
 
 def _confussion_decrypt(binary_char1: str, binary_char2: str, key_length: int) -> str:
@@ -43,6 +40,13 @@ def _confussion_decrypt(binary_char1: str, binary_char2: str, key_length: int) -
     xor_result = xor_binary_values(binary_char2, divided_result)
     
     return xor_result
+
+def _diffusion_encrypt_transposition(binary_msg: str, key: str):
+    
+    kn = len(key)
+    result = binary_msg[kn:] + binary_msg[:kn]
+    
+    return result
 
 def _diffusion_decrypt_shift(encrypted_message: str, charKey: str):
     
@@ -54,35 +58,10 @@ def _diffusion_decrypt_shift(encrypted_message: str, charKey: str):
     shifts = (shifts - 8 if shifts >= 9 else shifts)
 
     binary_msg = shift(encrypted_message, shifts)
-    return binary_msg
+    return encrypted_message
 
-def _diffusion_decrypt_rotation(binary_msg: str, key: str):
-    
-    # We get all concatenation of values
-    fullBinaryMsg = list(''.join(binary_msg))
-
-    n = str(int(transform_char_to_ascii(key[-1]),2))
-    kn = 0
-    kn = sum(int(n[i]) for i in range(len(n)))
-    kn = int(str(kn)[-1])
-
-    rotatedBinary = (fullBinaryMsg[:kn] + fullBinaryMsg[kn:])
-    result = ''.join(rotatedBinary)
-    print(result, fullBinaryMsg)
-    return 1
-
-# Shift left
+# Shift right (decrypt)
 def shift(binary_string: str, shifts: int):
-    #print(binary_string)
-    binary_string = str(format(int(binary_string,2), '08b'))
-    
-    #print(binary_string)
-    firstPart = binary_string[:-shifts:]
-    rest = binary_string[-shifts:]
-    shifted = rest + firstPart
-    #print(shifted)
-   
-    shifted_binary = bin(int(shifted, 2))
-    #print(shifted_binary)
-    return shifted_binary
+
+    return 1
 
--- src/algorithms/encrypt.py
+++ src/algorithms/encrypt.py
@@ -12,23 +12,18 @@ def encrypt_message(plain_text: str, key: str) -> str:
         
     for char in plain_text:
         plain_text_ascii.append(transform_char_to_ascii(char))
-        
-    # Uncomment this to see in the console the ORDER
-    # in which the operations are being done
-    # for char_message in plain_text:
-    #     for key_char in key:
-    #         print(char_message, key_char)
     
     for char_message in plain_text_ascii:
         operator = char_message
         for char_key in key_ascii:
             confussion_result_encrypt = _confussion_encrypt(operator, char_key, len(key))
-            diffusion_result_encrypt = _diffusion_encrypt_shift(confussion_result_encrypt, char_key)
             
             operator = confussion_result_encrypt # cambiar a diffusion_result_encrypt
 
         binary_array_result.append(operator)
-    result_encrypt = _diffusion_encrypt_rotation(binary_array_result, key)
+    
+    # Apply transposition layer encryption
+    result_encrypt = _diffusion_encrypt_transposition(binary_array_result, key) 
     base64_result = base64.b64encode(str(result_encrypt).encode('utf-8'))
     
     return binary_array_result, base64_result
@@ -37,7 +32,13 @@ def _confussion_encrypt(binary_char1: str, binary_char2: str, key_length: int):
     xor_result = xor_binary_values(binary_char1, binary_char2)
     multiplied_result = multiply_binary(xor_result, key_length)
     return multiplied_result
-        
+
+def _diffusion_encrypt_transposition(binary_msg: str, key: str):
+    
+    kn = len(key)
+    result = binary_msg[-kn:] + binary_msg[:-kn]
+    
+    return result
 
 def _diffusion_encrypt_shift(binary_msg: str, charKey: str):
 
@@ -49,40 +50,11 @@ def _diffusion_encrypt_shift(binary_msg: str, charKey: str):
     shifts = (shifts - 8 if shifts >= 9 else shifts)
     
     shifted_binary = shift(binary_msg, shifts)
-    #print("----------")
-    #print(shifts)
-    #print(binary_msg)
-    #print(shifted_binary)
-    #print("----------")
-    return shifted_binary
 
-def _diffusion_encrypt_rotation(binary_msg: str, key: str):
-    
-    # We get all concatenation of values
-    fullBinaryMsg = list(''.join(binary_msg))
+    return binary_msg
 
-    n = str(int(transform_char_to_ascii(key[-1]),2))
-    kn = 0
-    kn = sum(int(n[i]) for i in range(len(n)))
-    kn = int(str(kn)[-1])
-
-    
-    rotatedBinary = (fullBinaryMsg[kn:] + fullBinaryMsg[:kn])
-    result = ''.join(rotatedBinary)
-    print(result, fullBinaryMsg)
-    return result
-
-# Shift right
+# Shift left
 def shift(binary_string: str, shifts: int):
-    #print(binary_string)
-    binary_string = str(format(int(binary_string,2), '08b'))
     
-    #print(binary_string)
-    firstPart = binary_string[:shifts]
-    rest = binary_string[shifts:]
-    shifted = rest + firstPart
-    #print(shifted)
-    
-    shifted_binary = bin(int(shifted, 2))
-    #print(shifted_binary)
-    return shifted_binary
+    print(binary_string, char_key)
+    return 1
